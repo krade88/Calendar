972de51b6bd68669458983018998dad8f447079c
krade88 <71315629+krade88@users.noreply.github.com>
2022-12-21 16:14:35 +0300

    Add files via upload

---
+++ AdventCalendar2/AdventCalendar2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventCalendar2
+//
+//  Created by Хачатрян Рафаель Анушаванович on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        
+       
+    }
+    
+
+    }
+    
+    
+
+
